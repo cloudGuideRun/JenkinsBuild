6907e4f6fb7e956b852ca8902275bbff06ac3068
TAPAS BHATTACHARYA <tapas.bhattacharya@ericsson.com>
2020-02-06 15:17:46 +0530

    first commit

---
+++ WelcomeJava.java
@@ -0,0 +1,15 @@
+package jenkinsTraning;
+
+public class WelcomeJava {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10;i++) {
+			
+			System.out.println("Welcome to Java");
+			Thread.sleep(2000);
+		}
+
+	}
+
+}
